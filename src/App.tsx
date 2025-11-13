ff1cd7405ee257d71da430207142f44e9218ecb2
zayennn <zaayeennn@gmail.com>
2025-11-13 15:06:26 +0700

    updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from './contexts/ThemeContext';
 import Landing from './pages/Landing';
+import FilmDescription from './pages/FilmDescription';
 import './index.css';
 
 const App: React.FC = () => {
   return (
     <ThemeProvider>
-      <div className="App">
-        <Landing />
-      </div>
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/movie/:id" element={<FilmDescription />} />
+          </Routes>
+        </div>
+      </Router>
     </ThemeProvider>
   );
 };
